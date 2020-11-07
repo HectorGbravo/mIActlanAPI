953755fa529e56ae68366a540c3d85ef1e3f421d
Miguel Angel Morgado Huerta <miguelmorgado678@gmail.com>
2020-11-06 22:58:15 -0600

    Se agrego el DB Context, y el ConnectionString

---
+++ MiactlanAPI/Context/MiactlanDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiactlanAPI.Context
+{
+    public class MiactlanDbContext : DbContext
+    {
+        public MiactlanDbContext(DbContextOptions<MiactlanDbContext> options) : base(options)
+        {
+        }
+    }
+}
